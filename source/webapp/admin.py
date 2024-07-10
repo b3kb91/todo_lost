9a978ec8a26339eadf7608e149359a3d12bbaec8
Bekbol <19kani92@gmail.com>
2024-07-11 00:05:50 +0600

    добавил типы и статусы в админку

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -3,6 +3,24 @@ from django.contrib import admin
 from webapp.models import Issue
 
 
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_filter = ['title']
+    list_display_links = ["title"]
+    search_fields = ['title']
+    fields = ['title']
+    readonly_fields = ['title']
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_filter = ['title']
+    list_display_links = ["title"]
+    search_fields = ['title']
+    fields = ['title']
+    readonly_fields = ['title']
+
+
 class IssueAdmin(admin.ModelAdmin):
     list_display = ['id', 'description', 'statuses', 'summary']
     list_filter = ['statuses', 'id', 'types']
